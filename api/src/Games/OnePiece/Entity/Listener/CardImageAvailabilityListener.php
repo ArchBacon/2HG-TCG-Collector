a72070f3ddd3e5d15412c8b02171a115293c9354
Christian Delicaat <cdelicaat.me@gmail.com>
2026-09-24 14:24:01 +0200

    Added missing one piece card image availability listener. Card images were marked as missing in the api results without it.

---
+++ api/src/Games/OnePiece/Entity/Listener/CardImageAvailabilityListener.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\Games\OnePiece\Entity\Listener;
+
+use App\Enum\Game;
+use App\Games\OnePiece\Entity\Card;
+use App\Service\CardImageService;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Events;
+
+#[AsEntityListener(event: Events::postLoad, method: 'postLoad', entity: Card::class)]
+final readonly class CardImageAvailabilityListener
+{
+    public function __construct(private CardImageService $images) {}
+
+    public function postLoad(Card $card): void
+    {
+        $card->hasImages = $this->images->hasAllSizes(
+            Game::OnePiece,
+            $card->id->toRfc4122()
+        );
+    }
+}
